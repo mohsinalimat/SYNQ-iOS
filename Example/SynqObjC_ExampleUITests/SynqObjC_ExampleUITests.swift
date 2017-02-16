f3aeff4859b752f3c330bcc1a38c3ac68c431355
Kjartan <kjartanvestvik@gmail.com>
2017-02-16 11:21:22 +0100

    Update SynqStreamer.framework. Add architecture check to UI tests to allow testing SynqStreamer on simulator

--- Example/SynqObjC_ExampleUITests/SynqObjC_ExampleUITests.swift
+++ Example/SynqObjC_ExampleUITests/SynqObjC_ExampleUITests.swift
@@ -18,8 +18,18 @@ class SynqObjC_ExampleUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
+        let app = XCUIApplication()
+        
+        // Check architecture to determine if running on simulator
+        // If so, set env variable to stop camera from being started in SynqStreamer
+        #if (arch(i386) || arch(x86_64)) && os(iOS)
+            var launchEnv = app.launchEnvironment
+            launchEnv["UITesting"] = "YES"
+            app.launchEnvironment = launchEnv
+        #endif
+        
+        app.launch()
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
@@ -44,27 +54,32 @@ class SynqObjC_ExampleUITests: XCTestCase {
         XCTAssert(app.buttons["Upload"].exists)
         XCTAssert(app.buttons["Open StreamView"].exists)
     }
-    /*
+    
     func testStreamer() {
         
         let app = XCUIApplication()
         app.buttons["Open StreamView"].tap()
         
-        addUIInterruptionMonitor(withDescription: "Camera Dialog") { (alert) -> Bool in
-            alert.buttons["OK"].tap()
-            return true
-        }
+        #if (!arch(i386) && !arch(x86_64)) && os(iOS)
+            addUIInterruptionMonitor(withDescription: "Camera Dialog") { (alert) -> Bool in
+                alert.buttons["OK"].tap()
+                return true
+            }
+            
+            addUIInterruptionMonitor(withDescription: "Microphone Dialog") { (alert) -> Bool in
+                alert.buttons["OK"].tap()
+                return true
+            }
+            
+            app.tap()
+            
+            XCTAssert(app.buttons["icSwitchCams"].exists)
+            XCTAssert(app.buttons["icMicOff"].exists)
+        #endif
         
-        addUIInterruptionMonitor(withDescription: "Microphone Dialog") { (alert) -> Bool in
-            alert.buttons["OK"].tap()
-            return true
-        }
         
-        app.tap()
-        
-        XCTAssert(app.buttons["icRecStart"].exists)
         XCTAssert(app.buttons["icSettings"].exists)
         XCTAssert(app.buttons["icCloseWhite"].exists)
     }
-    */
+    
 }
